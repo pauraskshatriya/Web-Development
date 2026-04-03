345f58aeaae91e754626b110b6719bb46205d3ee
Pauras Kshatriya <pauraskshatriya@gmail.com>
2026-04-03 22:46:00 +0530

    Update conditions.js

--- jsconditions/conditions.js
+++ jsconditions/conditions.js
@@ -1,22 +1,30 @@
-console.log("Hello I am conditional tutorial");
+console.log("Conditional Tutorial");
 
 let age = 1;
 
-if (age === 18) {
-    console.log("You can drive");
-}
-else if (age === 0) {
-    console.log("Are you kidding?");
-}
-else if (age === 1) {
-    console.log("Are you again kidding?");
-}
-else {
-    console.log("You cannot drive");
+// Function for better structure & reusability
+function checkDrivingEligibility(age) {
+    if (age === 18) {
+        console.log("You can drive");
+    } 
+    else if (age <= 0) {
+        console.log("Invalid age. Are you kidding?");
+    } 
+    else if (age === 1) {
+        console.log("You're too young to even ask 😄");
+    } 
+    else {
+        console.log("You cannot drive");
+    }
 }
 
+checkDrivingEligibility(age);
+
+// Find absolute difference between two numbers
 let a = 6;
 let b = 8;
 
-let c = a > b ? (a - b) : (b - a);
-console.log(c);
+// Cleaner approach using Math.abs()
+let difference = Math.abs(a - b);
+
+console.log("Difference:", difference);
